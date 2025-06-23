fa6ad4112f4fdf8c2edaf18def921014ba89b8ee
Amol M <117634099+Amol5766@users.noreply.github.com>
2025-06-24 04:16:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
-  plugins: [angular()],
   server: {
     host: true,
     port: 4200,
     strictPort: true,
-    allowedHosts: ['6kcwdw-4200.csb.app', 'localhost'],
+    allowedHosts: ['all'],
   }
 });
